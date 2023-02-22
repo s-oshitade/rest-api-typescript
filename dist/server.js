82faf49143d5b0843f6a2a232e694de648ab37f4
Seun Oshitade <seun.oshitade@gmail.com>
2023-02-21 21:43:46 -0700

    Implement a data retrieval express endpoint

--- dist/server.js
+++ dist/server.js
@@ -14,9 +14,11 @@ var root_1 = require("./routes/root");
 var utils_1 = require("./utils");
 var logger_1 = require("./logger");
 var data_source_1 = require("./data-source");
+var get_all_courses_1 = require("./routes/get-all-courses");
 var app = express();
 function setupExpress() {
     app.route("/").get(root_1.root);
+    app.route("/api/courses").get(get_all_courses_1.getAllCourses);
 }
 function startServer() {
     var port;
